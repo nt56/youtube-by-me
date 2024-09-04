a5c50a037b5c1e09329db799ccbe56b75e536103
nt_56 <nagutirth2121@gmail.com>
2024-09-04 14:03:08 +0530

    git ignore updated

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+const GOOGLE_API_KEY = process.env.GOOGLE_YOUTUBE_API_KEY;
+
+export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
+
+export const YOUTUBE_VIDEO_BYID = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=${GOOGLE_API_KEY}&id=`;
+
+export const YOUTUBE_SEARCH_API = `http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
+
+export const SEARCH_RESULT_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${GOOGLE_API_KEY}&q=`;
+
+export const COMMENTS_API = `https://www.googleapis.com/youtube/v3/commentThreads?textFormat=plainText&part=snippet&maxResults=50&key=${GOOGLE_API_KEY}&videoId=`;
+
+export const OFFSET_LIVE_CHAT = 20;
