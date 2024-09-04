f62bdb6a6834994fb69cd5d8e3f89e4f293612d1
nt_56 <nagutirth2121@gmail.com>
2024-09-04 14:06:49 +0530

    done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = process.env.GOOGLE_YOUTUBE_API_KEY;
+const GOOGLE_API_KEY = "AIzaSyA4IwXcZ3qqTsTl-u4An0Z5-DmHZ-hhXME";
 
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
